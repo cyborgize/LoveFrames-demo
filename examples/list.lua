644840eb8163ab55e3ad663b12883ac3e2b6f951
Kenny Shields <nikolairesokav@gmail.com>
2014-01-28 11:06:17 -0500

    Fix text positioning issues in list example

--- examples/list.lua
+++ examples/list.lua
@@ -2,6 +2,8 @@ local example = {}
 example.title = "List"
 example.category = "Object Demonstrations"
 
+local font = love.graphics.newFont(10)
+
 function example.func()
 	
 	local frame = loveframes.Create("frame")
@@ -19,6 +21,8 @@ function example.func()
 	panel:SetSize(490, 115)
 	panel.Draw = function() end
 	
+	local text1 = loveframes.Create("text", panel)
+	local text2 = loveframes.Create("text", panel)
 	local slider1 = loveframes.Create("slider", panel)
 	slider1:SetPos(5, 20)
 	slider1:SetWidth(480)
@@ -28,20 +32,20 @@ function example.func()
 	slider1:SetDecimals(0)
 	slider1.OnValueChanged = function(object2, value)
 		list:SetPadding(value)
+		text2:SetPos(slider1:GetWidth() - text2:GetWidth(), 5)
+		text2:SetText(slider1:GetValue())
 	end
 		
-	local text1 = loveframes.Create("text", panel)
 	text1:SetPos(5, 5)
-	text1:SetFont(love.graphics.newFont(10))
+	text1:SetFont(font)
 	text1:SetText(slider1:GetText())
-		
-	local text2 = loveframes.Create("text", panel)
-	text2:SetFont(love.graphics.newFont(10))
-	text2.Update = function(object, dt)
-		object:SetPos(slider1:GetWidth() - object:GetWidth(), 5)
-		object:SetText(slider1:GetValue())
-	end
-		
+	
+	text2:SetText(slider1:GetValue())
+	text2:SetFont(font)
+	text2:SetPos(slider1:GetWidth() - text2:GetWidth(), 5)
+	
+	local text3 = loveframes.Create("text", panel)
+	local text4 = loveframes.Create("text", panel)
 	local slider2 = loveframes.Create("slider", panel)
 	slider2:SetPos(5, 60)
 	slider2:SetWidth(480)
@@ -51,20 +55,18 @@ function example.func()
 	slider2:SetDecimals(0)
 	slider2.OnValueChanged = function(object2, value)
 		list:SetSpacing(value)
+		text4:SetPos(slider2:GetWidth() - text4:GetWidth(), 45)
+		text4:SetText(slider2:GetValue())
 	end
 		
-	local text3 = loveframes.Create("text", panel)
 	text3:SetPos(5, 45)
-	text3:SetFont(love.graphics.newFont(10))
+	text3:SetFont(font)
 	text3:SetText(slider2:GetText())
-		
-	local text4 = loveframes.Create("text", panel)
-	text4:SetFont(love.graphics.newFont(10))
-	text4.Update = function(object, dt)
-		object:SetPos(slider2:GetWidth() - object:GetWidth(), 45)
-		object:SetText(slider2:GetValue())
-	end
-		
+	
+	text4:SetText(slider2:GetValue())
+	text4:SetFont(font)
+	text4:SetPos(slider2:GetWidth() - text4:GetWidth(), 45)
+	
 	local button1 = loveframes.Create("button", panel)
 	button1:SetPos(5, 85)
 	button1:SetSize(237, 25)
